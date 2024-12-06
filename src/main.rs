e94b6381e2b2d1838fb14d4ed414c20fe1fd571a
Snow-blip <yixue.wang@proton.me>
2024-12-06 20:32:45 +0100

    added reading lines and error propagation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::error;
+use std::fmt::Error;
+use std::io::{self, BufRead};
 use std::fs;
 use std::env;
 
@@ -5,17 +8,21 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "1");
 
     let example_solution = 11;
-    assert_eq!(findstar("src/example"),example_solution);
+    assert_eq!(findstar("src/example").unwrap(),example_solution);
     println!("Found the correct example solution {example_solution}.");
 
-    let answer = findstar("src/input");
+    let answer = findstar("src/input").unwrap();
     println!("Answer: {answer}");
 }
 
-fn findstar(file_path: &str) -> isize {
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-    let solution: isize;
-    solution = 11;
-    solution
+fn findstar(file_path: &str) -> Result<isize,Box<dyn error::Error>> {
+    //let contents = fs::read_to_string(file_path)
+    //    .expect("Should have been able to read the file");
+
+    let file = fs::File::open(file_path)?;
+    let lines = io::BufReader::new(file).lines();
+
+
+    let solution = 11;
+    Ok(solution)
 }
